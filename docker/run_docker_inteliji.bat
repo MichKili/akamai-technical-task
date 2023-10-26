34478b272d11298f80fadaa2fc9ad693a2882f6f
Michal Kilian <m.kilian@ambsoft.pl>
2023-10-27 00:09:52 +0200

    add run_docker_inteliji.bat for inteliji configuration external tools v2

--- docker/run_docker_inteliji.bat
+++ docker/run_docker_inteliji.bat
@@ -2,9 +2,9 @@
 chcp 65001
 cmd /c start "" /wait bash -ic "export BROWSER='/mnt/c/Program Files/Google/Chrome/Application/chrome.exe' && cd '/mnt/c/Users/MichaÅ‚/IdeaProjects/giftcards-service/docker' && ./run_docker.sh %1 %2"
 
-::echo and chcp is used to properly write polish sign like ³ 
-:: cmd /c - > przekazuje comendy do cmd.exe i wtedy zamyka siê okno
-:: start "" /wait -> tworzy nowe okno które czeka /wait na wykonanie siê ca³ego zadania i wspó³pracuj¹c z /c zamyka dopiero wtedy siê nowo otwarte okno.
-:: bash -ic -> odpala interaktywny bash from wsl który wykonywaæ mo¿e wszelkie komendy (c) np scrypty sh, interaktywny oznacza ¿e coœ tam z profilami jest lepiej ustawione, np ¿e bash pobiera dane meta, jakieœ ustawienia z Windowsa czy coœ takiego
-:: export ... wyeksrptowuje browser jako zmienna systemowa aby wiadomo by³o jaka przegl¹darka ma siê wywo³aæ podczas wywo³ywania skryptu run_dicker.sh
-:: ./run_docker.sh %1 %2 wywo³anie skryptu z dwoma mo¿liwymi argumentami które mo¿emy pdoaæ na wejœcie do tego algorytmu.
+::echo and chcp is used to properly write polish sign like Å‚
+:: cmd /c - > przekazuje comendy do cmd.exe i wtedy zamyka siÄ™ okno
+:: start "" /wait -> tworzy nowe okno ktÃ³re czeka /wait na wykonanie siÄ™ caÅ‚ego zadania i wspÃ³pracujÄ…c z /c zamyka dopiero wtedy siÄ™ nowo otwarte okno.
+:: bash -ic -> odpala interaktywny bash from wsl ktÃ³ry wykonywaÄ‡ moÅ¼e wszelkie komendy (c) np scrypty sh, interaktywny oznacza Å¼e coÅ› tam z profilami jest lepiej ustawione, np Å¼e bash pobiera dane meta, jakieÅ› ustawienia z Windowsa czy coÅ› takiego
+:: export ... wyeksrptowuje browser jako zmienna systemowa aby wiadomo byÅ‚o jaka przeglÄ…darka ma siÄ™ wywoÅ‚aÄ‡ podczas wywoÅ‚ywania skryptu run_dicker.sh
+:: ./run_docker.sh %1 %2 wywoÅ‚anie skryptu z dwoma moÅ¼liwymi argumentami ktÃ³re moÅ¼emy podaÄ‡ na wejÅ›cie do tego algorytmu.
